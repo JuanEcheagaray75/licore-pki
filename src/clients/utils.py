a93bcaba1ebdb2cb7e3b6b1deaba0e6cb1a0d1b4
JuanEcheagaray75 <a00830646@tec.mx>
2023-05-21 19:26:28 -0600

    chore: renamed create packets to store general utilities

--- src/clients/utils.py
+++ src/clients/utils.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import time
 
 
 def create_packets(csv_file: str) -> list:
@@ -11,19 +10,3 @@ def create_packets(csv_file: str) -> list:
     df.sort_values(by=['datetime'], inplace=True)
 
     return df['packet'].tolist()
-
-
-def test_send_packets(packets: list) -> None:
-    for packet in packets:
-        print(packet)
-        time.sleep(1)
-
-
-def main():
-
-    packets = create_packets('db/processed/Prosumer_ABC.csv')
-    test_send_packets(packets)
-
-
-if __name__ == '__main__':
-    main()
