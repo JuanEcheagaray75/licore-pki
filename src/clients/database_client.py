65611972df650bbd66674c11463e4ff79f796785
JuanEcheagaray75 <a00830646@tec.mx>
2023-05-24 00:06:44 -0600

    feat: added bidirectional communication channel. Found python ECDSA does chop digest if it's too long

--- src/clients/database_client.py
+++ src/clients/database_client.py
@@ -1,12 +1,31 @@
 import paho.mqtt.client as mqtt
+from paho.mqtt.publish import single
 import sqlite3
 from pathlib import Path
 from cryptography.hazmat.primitives.asymmetric.ec import ECDSA
 from cryptography.hazmat.primitives.hashes import BLAKE2b
 from cryptography.x509 import load_pem_x509_certificate
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.x509.oid import NameOID
+
+
+# Load Certificates
+with open('../.test_certs/database_licore.key', 'rb') as f:
+    database_key = load_pem_private_key(f.read(), password=None)
+
+with open('../.test_certs/database_licore.crt', 'rb') as f:
+    database_cert = load_pem_x509_certificate(f.read())
+    pub_key = database_cert.public_key()
+
 
 
 def write_to_db(db_path: str, packet: list) -> None:
+    """Write auditor packet to sqlite db
+
+    Args:
+        db_path (str): Relative path to sqlite3 database
+        packet (list): List representation of packet
+    """
     try:
         sqliteConnection = sqlite3.connect(db_path)
         cursor = sqliteConnection.cursor()
@@ -35,8 +54,18 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("audits/measures/#", qos=0)
 
 
-def on_message(client, userdata, msg):
-    
+
+def on_message(client: mqtt, userdata, msg):
+    """On message utility of MQTT publisher client
+
+    Args:
+        client (mqtt): MQTT client
+        userdata (_type_): Userdata representation of client
+        msg (_type_): Message to publish
+
+    Raises:
+        Exception: Invalid
+    """
 
     received_message = msg.payload.decode('utf-8')
     user_id = msg.topic.split('/')[2]
@@ -62,9 +91,13 @@ def on_message(client, userdata, msg):
         except:
             raise Exception('Invalid signature')
 
+# GENERAL PARAMETERS
+broker = "mqtt.mona-temp-test.com"
+port = 8883
+id = database_cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
 
 
-client = mqtt.Client("database_licore", clean_session=False)
+client = mqtt.Client(id, clean_session=False)
 client.tls_set(certfile='../.test_certs/database_licore.crt',
                keyfile='../.test_certs/database_licore.key',
                ca_certs='../.test_certs/ca-root-cert.crt')
@@ -72,5 +105,23 @@ client.tls_set(certfile='../.test_certs/database_licore.crt',
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("mqtt.mona-temp-test.com", 8883, 60)
+client.connect(broker, port, 60)
+
+# Test message of parameter update for bidirectional communication proof of concept
+tls_sets = {'ca_certs':"../.test_certs/ca-root-cert.crt",
+            'certfile':"../.test_certs/database_licore.crt",
+            'keyfile':"../.test_certs/database_licore.key"}
+
+test_message = 'hello auditor'
+signature = database_key.sign(
+        test_message.encode('utf-8'),
+        ECDSA(BLAKE2b(64))
+    )
+packet = test_message + "||" + signature.hex()
+
+single(topic='control_center/updates/database_licore',
+       payload=packet, qos=0,
+       hostname=broker, port = port,
+       tls=tls_sets, retain=True)
+
 client.loop_forever(timeout=30, max_packets=3000)
--- src/clients/publisher.py
+++ src/clients/publisher.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 from time import sleep
+from pathlib import Path
 from cryptography.hazmat.primitives.asymmetric.ec import ECDSA
 from cryptography.hazmat.primitives.hashes import BLAKE2b
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
@@ -8,6 +9,12 @@ from cryptography.x509.oid import NameOID
 from utils import create_packets
 
 
+# TODO: Change this name accordingly, for dev purposes we use
+# the python database client script to test the general sending
+# of parameter updates.
+CONTROL_CENTER_NAME = 'database_licore'
+
+# Load Certificates
 with open('../.test_certs/auditor_mona.key', 'rb') as f:
     auditor_key = load_pem_private_key(f.read(), password=None)
 
@@ -15,10 +22,51 @@ with open('../.test_certs/auditor_mona.crt', 'rb') as f:
     auditor_cert = load_pem_x509_certificate(f.read())
     pub_key = auditor_cert.public_key()
 
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected through MQTT " + str(rc))
+    client.subscribe("control_center/updates/#", qos=0)
+
+
+def on_message(client: mqtt, userdata, msg):
+    """On message utility of MQTT publisher client
+
+    Args:
+        client (mqtt): MQTT client
+        userdata (_type_): Userdata representation of client
+        msg (_type_): Message to publish
+
+    Raises:
+        Exception: Invalid
+    """
+
+    received_message = msg.payload.decode('utf-8')
+    user_id = msg.topic.split('/')[2]
+    packet = received_message.split('||')[0]
+    signature = bytes.fromhex(received_message.split('||')[-1])
+
+
+    if Path(f'../.test_certs/{user_id}.crt').is_file() and user_id == CONTROL_CENTER_NAME:
+
+        with open(f'../.test_certs/{user_id}.crt', 'rb') as f:
+            user_cert = load_pem_x509_certificate(f.read())
+            pub_key = user_cert.public_key()
+
+        try:
+            pub_key.verify(signature,
+                           packet.encode('utf-8'),
+                           ECDSA(BLAKE2b(64)))
+
+            print(f'Received parameter update as: {packet}')
+        except:
+            raise Exception('Invalid signature')
+
+
 # GENERAL PARAMETERS
 broker = "mqtt.mona-temp-test.com"
 port = 8883
 id = auditor_cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+
 
 # Client setup
 client = mqtt.Client("auditor_mona", clean_session=False)
@@ -26,14 +74,24 @@ client.tls_set(certfile='../.test_certs/auditor_mona.crt',
                keyfile='../.test_certs/auditor_mona.key',
                ca_certs='../.test_certs/ca-root-cert.crt')
 
+client.on_connect = on_connect
+client.on_message = on_message
+
 client.tls_insecure_set(False)
 client.connect(broker, port, 2*60)
 
 packets = create_packets('../../db/processed/Prosumer_ABC.csv')
 
+client.loop_start()
 print('Publishing packets...')
 for packet in packets:
 
+    # IMPORTANT: Hash length of Blake2b
+    # Signature algorithm is in pair with FIPS 186-4
+    # https://csrc.nist.gov/publications/detail/fips/186/4/final
+    # The source code is written in Rust, it does chop the hash, if
+    # the len of the digest is greater than 256 bits
+    # The standard imposes this behavior
     signature = auditor_key.sign(
         packet.encode('utf-8'),
         ECDSA(BLAKE2b(64))
@@ -41,10 +99,11 @@ for packet in packets:
     payload = packet + '||' + signature.hex()
     client.publish(topic=f'audits/measures/{id}',
                    payload=payload,
-                   qos=0)
+                   qos=2)
 
     print(f'Packet {packet} published')
     sleep(0.2)
 
 print('Done!')
+client.loop_stop()
 client.disconnect()
